d776b2f9d0f7bebe43779a5c6505ae9aaad074f7
DL200010 <dl200010@gmail.com>
2018-12-19 14:34:05 -0600

    Insert missing licensing blocks

--- elements/dsp_elements.php
+++ elements/dsp_elements.php
@@ -1,4 +1,8 @@
-
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
 			<!-- Header -->
 				<header id="header">
 					<a href="?action=home.home" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
