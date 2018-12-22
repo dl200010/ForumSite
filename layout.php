e29750a5c7edd124c6c3bb90c37fabae31eb9fd1
DL200010 <dl200010@gmail.com>
2018-12-21 23:33:20 -0600

    Include additional methods for jquery validation

--- layout.php
+++ layout.php
@@ -58,6 +58,7 @@
 		<!-- <script src="/assets/lobibox/1.2.7/js/messageboxes.min.js"></script> -->
 		<!-- <script src="/assets/lobibox/1.2.7/js/notifications.min.js"></script> -->
 		<script src="/assets/jquery-validation/1.19.0/jquery.validate.min.js"></script>
+		<script src="/assets/jquery-validation/1.19.0/additional-methods.min.js"></script>
 
 		<?php
 			if (isset($flexaction['page_js_files']) && $flexaction['page_js_files'] != "") {
