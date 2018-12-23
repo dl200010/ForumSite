b3c376659e6b4766611a53ff3e565fee045ad755
DL200010 <dl200010@gmail.com>
2018-12-22 23:30:02 -0600

    Use PHP Comments for licensing

--- elements/dsp_elements.php
+++ elements/dsp_elements.php
@@ -1,8 +1,10 @@
-<!--
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
+<?php
+	/*
+	 *  Editorial by HTML5 UP
+	 *  html5up.net | @ajlkn
+	 *  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+	 */
+?>
 	<!-- Header -->
 		<header id="header">
 			<a href="?action=home.home" class="logo"><strong>Editorial</strong> by HTML5 UP</a>
--- layout.php
+++ layout.php
@@ -14,13 +14,14 @@
 	 *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 	 *  See the License for the specific language governing permissions and
 	 *  limitations under the License.
+	 *
+	 *
+	 *
+	 *  Editorial by HTML5 UP
+	 *  html5up.net | @ajlkn
+	 *  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 	 */
 ?>
-<!--
-	Editorial by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>The Forums</title>
